82cdb93aa8e70b112a10cf204902eb56b445f21f
Tom French <tom@tomfren.ch>
2020-06-23 20:26:31 +0100

    feat: add button to start new payout round

--- src/components/PayoutList/index.tsx
+++ src/components/PayoutList/index.tsx
@@ -1,11 +1,20 @@
 import React, { useMemo, useState, useEffect } from "react";
-import { TableRow, TableCell } from "@material-ui/core";
 import { formatUnits } from "ethers/utils";
+
+import { TableRow, TableCell } from "@material-ui/core";
+import { Text, Icon } from "@gnosis.pm/safe-react-components";
+import styled from "styled-components";
+
 import Table from "../common/StyledTable";
 import PayoutModal from "../Modals/PayoutModal";
 import { Token } from "../../typings";
 import { useColonyClient } from "../../contexts/ColonyContext";
 import { REWARDS_FUNDING_POT_ID } from "../../constants";
+
+const UnderlinedTableRow = styled(TableRow)`
+  border-bottom-width: 3px;
+  border-bottom-style: solid;
+`;
 
 const TokenRow = ({ token }: { token: Token }) => {
   const colonyClient = useColonyClient();
@@ -31,10 +40,31 @@ const TokenRow = ({ token }: { token: Token }) => {
   );
 };
 
+const NewPayoutRow = () => {
+  // const [isOpen, setIsOpen] = useState<boolean>(false);
+  return (
+    <>
+      <UnderlinedTableRow onClick={() => console.log("Opening New Payout modal")}>
+        <TableCell>
+          <Text size="lg">New Payout</Text>
+        </TableCell>
+        <TableCell align="right">
+          <Icon type="add" size="md" />
+        </TableCell>
+      </UnderlinedTableRow>
+    </>
+  );
+};
+
 const PayoutList = ({ tokens }: { tokens: Token[] }) => {
   const tokenList = useMemo(() => tokens.map(token => <TokenRow token={token} />), [tokens]);
 
-  return <Table>{tokenList}</Table>;
+  return (
+    <Table>
+      <NewPayoutRow />
+      {tokenList}
+    </Table>
+  );
 };
 
 export default PayoutList;
