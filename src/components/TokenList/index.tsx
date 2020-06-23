e5bebbf0fb5d06262e3836d2f2c20f02307f89d6
Tom French <tom@tomfren.ch>
2020-06-23 19:15:22 +0100

    feat: add all required permissions for TokenList to pass to modal

--- src/components/TokenList/index.tsx
+++ src/components/TokenList/index.tsx
@@ -8,11 +8,28 @@ import { useHasDomainPermission } from "../../contexts/ColonyContext";
 import { useSafeInfo } from "../../contexts/SafeContext";
 import { Token } from "../../typings";
 
-const TokenRow = ({ token, hasFundingRole }: { token: Token; hasFundingRole: boolean }) => {
+const TokenRow = ({
+  token,
+  hasRootRole,
+  hasAdministrationRole,
+  hasFundingRole,
+}: {
+  token: Token;
+  hasRootRole: boolean;
+  hasAdministrationRole: boolean;
+  hasFundingRole: boolean;
+}) => {
   const [isOpen, setIsOpen] = useState<boolean>(false);
   return (
     <>
-      <TokenModal isOpen={isOpen} setIsOpen={setIsOpen} token={token} hasFundingRole={hasFundingRole} />
+      <TokenModal
+        isOpen={isOpen}
+        setIsOpen={setIsOpen}
+        token={token}
+        hasRootRole={hasRootRole}
+        hasAdministrationRole={hasAdministrationRole}
+        hasFundingRole={hasFundingRole}
+      />
       <TableRow onClick={() => setIsOpen(true)}>
         <TableCell>{token.symbol}</TableCell>
         <TableCell align="right">0.000</TableCell>
@@ -23,11 +40,21 @@ const TokenRow = ({ token, hasFundingRole }: { token: Token; hasFundingRole: boo
 
 const TokenList = ({ tokens }: { tokens: Token[] }) => {
   const safeInfo = useSafeInfo();
+  const hasRootPermission = useHasDomainPermission(safeInfo?.safeAddress, 1, ColonyRole.Root);
+  const hasAdministrationPermission = useHasDomainPermission(safeInfo?.safeAddress, 1, ColonyRole.Administration);
   const hasFundingPermission = useHasDomainPermission(safeInfo?.safeAddress, 1, ColonyRole.Funding);
 
   const tokenList = useMemo(
-    () => tokens.map(token => <TokenRow token={token} hasFundingRole={hasFundingPermission} />),
-    [tokens, hasFundingPermission],
+    () =>
+      tokens.map(token => (
+        <TokenRow
+          token={token}
+          hasRootRole={hasRootPermission}
+          hasAdministrationRole={hasAdministrationPermission}
+          hasFundingRole={hasFundingPermission}
+        />
+      )),
+    [tokens, hasRootPermission, hasAdministrationPermission, hasFundingPermission],
   );
 
   return <Table>{tokenList}</Table>;
