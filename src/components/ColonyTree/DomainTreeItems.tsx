5850d1abb7d09adfdec419dde76934dc5ade420f
Tom French <tom@tomfren.ch>
2020-06-24 22:46:03 +0100

    style: typo fix startNoteId -> startNodeId

--- src/components/ColonyTree/DomainTreeItems.tsx
+++ src/components/ColonyTree/DomainTreeItems.tsx
@@ -3,15 +3,15 @@ import React from "react";
 import StyledTreeItem from "./StyledTreeItem";
 import { Domain } from "../../typings";
 
-const DomainTreeItems = ({ domains, startNoteId = 1 }: { domains: Domain[]; startNoteId?: number }) => {
+const DomainTreeItems = ({ domains, startNodeId = 1 }: { domains: Domain[]; startNodeId?: number }) => {
   if (domains.length === 0) return null;
-  if (domains.length === 1) return <StyledTreeItem nodeId={startNoteId.toString()} labelText="Root Domain" />;
+  if (domains.length === 1) return <StyledTreeItem nodeId={startNodeId.toString()} labelText="Root Domain" />;
 
   return (
-    <StyledTreeItem nodeId={startNoteId.toString()} labelText="Root Domain">
+    <StyledTreeItem nodeId={startNodeId.toString()} labelText="Root Domain">
       {domains.slice(1).map((_domain: Domain, index: number) => {
         const domainNumber = index + 1;
-        const nodeId = startNoteId + domainNumber;
+        const nodeId = startNodeId + domainNumber;
         return (
           // eslint-disable-next-line react/no-array-index-key
           <StyledTreeItem key={domainNumber} nodeId={nodeId.toString()} labelText={`Domain #${domainNumber}`} />
