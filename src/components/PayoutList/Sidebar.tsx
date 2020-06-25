18d57877a04bba29d1ef4709a222772f8b5ad6c8
Tom French <tom@tomfren.ch>
2020-06-25 12:25:45 +0100

    feat: display correct rewards percentage

--- src/components/PayoutList/Sidebar.tsx
+++ src/components/PayoutList/Sidebar.tsx
@@ -1,17 +1,21 @@
-import React from "react";
+import React, { useMemo } from "react";
 import { ColonyRole } from "@colony/colony-js";
 import { Button, Text } from "@gnosis.pm/safe-react-components";
-import { useHasDomainPermission } from "../../contexts/ColonyContext";
+import { BigNumber } from "ethers/utils";
+import { useHasDomainPermission, useRewardInverse } from "../../contexts/ColonyContext";
 import { useSafeInfo } from "../../contexts/SafeContext";
 import SetRewardsModal from "../Modals/SetRewardsModal";
 
 const PayoutSidebar = () => {
   const safeInfo = useSafeInfo();
+  const rewardsInverse = useRewardInverse();
   const hasRootPermission = useHasDomainPermission(safeInfo?.safeAddress, 1, ColonyRole.Root);
+
+  const rewardsPercentage = useMemo(() => new BigNumber(100).div(rewardsInverse), [rewardsInverse]);
 
   return (
     <>
-      <Text size="md">20% of all incoming revenue is contributed to the rewards pot</Text>
+      <Text size="md">{`${rewardsPercentage}% of all incoming revenue is contributed to the rewards pot`}</Text>
       <SetRewardsModal disabled={!hasRootPermission} />
       <Button size="md" color="primary" onClick={() => console.log("Opening Lock/Unlock Tokens")}>
         Lock/Unlock Tokens
