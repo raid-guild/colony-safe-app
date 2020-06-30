45ed7b3e307ae0659333af1f9cdf8450e98a1032
Tom French <tom@tomfren.ch>
2020-07-01 00:47:52 +0100

    feat: added rough implementation of setting rewards inverse

--- src/components/Modals/SetRewardsModal/index.tsx
+++ src/components/Modals/SetRewardsModal/index.tsx
@@ -1,9 +1,31 @@
-import React, { useState, ChangeEvent } from "react";
+import React, { useState, ChangeEvent, useCallback } from "react";
 import { Button, GenericModal, TextField, ModalFooterConfirmation } from "@gnosis.pm/safe-react-components";
+import { bigNumberify } from "ethers/utils";
+import setRewardInverseTxs from "../../../utils/transactions/setRewardInverse";
+import { useAppsSdk } from "../../../contexts/SafeContext";
+import { useColonyClient } from "../../../contexts/ColonyContext";
+import { MAX_U256 } from "../../../constants";
 
 const SetRewardsModal = ({ disabled }: { disabled?: boolean }) => {
+  const appsSdk = useAppsSdk();
+  const colonyClient = useColonyClient();
+
   const [isOpen, setIsOpen] = useState(false);
   const [rewardsPercentage, setRewardsPercentage] = useState<string>("");
+
+  const setRewardsInverse = useCallback(
+    (percentage: string) => {
+      if (colonyClient) {
+        const rewardsInverse = bigNumberify(
+          parseFloat(percentage) !== 0 ? 1 / (parseFloat(percentage) / 100) : MAX_U256,
+        );
+        console.log("setting inverse to", rewardsInverse);
+        const txs = setRewardInverseTxs(colonyClient, rewardsInverse);
+        appsSdk.sendTransactions(txs);
+      }
+    },
+    [colonyClient, appsSdk],
+  );
 
   const modalBody = (
     <>
@@ -21,7 +43,7 @@ const SetRewardsModal = ({ disabled }: { disabled?: boolean }) => {
       okText="Save"
       // okDisabled={false}
       handleCancel={() => setIsOpen(false)}
-      handleOk={() => console.log("Clicked ok!", rewardsPercentage)}
+      handleOk={() => setRewardsInverse(rewardsPercentage)}
     />
   );
   return (
