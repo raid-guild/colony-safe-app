26f94db63a7e3b190b0a7f4112a2fc9993b30476
Tom French <tom@tomfren.ch>
2020-07-11 18:18:44 +0100

    refactor: split getColonyTokensInfo into separate functions

--- src/utils/colony/getColonyTokensInfo.ts
+++ src/utils/colony/getColonyTokensInfo.ts
@@ -1,30 +1,32 @@
 import { getTokenClient, ColonyClient } from "@colony/colony-js";
 import { AddressZero } from "ethers/constants";
+import { Provider } from "ethers/providers";
 import getColonyTokens from "./getColonyTokens";
 import { Token } from "../../typings";
+
+const getTokenInfo = async (provider: Provider, tokenAddress: string): Promise<Token | undefined> => {
+  if (tokenAddress === AddressZero) {
+    return {
+      address: AddressZero,
+      name: "Ether",
+      symbol: "ETH",
+      decimals: 18,
+    };
+  }
+  try {
+    return {
+      address: tokenAddress,
+      ...(await getTokenClient(tokenAddress, provider).getTokenInfo()),
+    };
+  } catch (e) {
+    return undefined;
+  }
+};
 
 const getColonyTokensInfo = async (colonyClient: ColonyClient): Promise<Token[]> => {
   const tokenAddresses = await getColonyTokens(colonyClient);
   const tokens: (Token | undefined)[] = await Promise.all(
-    tokenAddresses.map(async (tokenAddress: string) => {
-      if (tokenAddress === AddressZero) {
-        return {
-          address: AddressZero,
-          name: "Ether",
-          symbol: "ETH",
-          decimals: 18,
-        };
-      }
-      try {
-        return {
-          address: tokenAddress,
-          ...(await getTokenClient(tokenAddress, colonyClient.provider).getTokenInfo()),
-        };
-      } catch (e) {
-        console.log(e);
-        return undefined;
-      }
-    }),
+    tokenAddresses.map(async (tokenAddress: string) => getTokenInfo(colonyClient.provider, tokenAddress)),
   );
 
   return tokens.filter(token => typeof token !== "undefined") as Token[];
